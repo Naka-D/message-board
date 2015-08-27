8503caf452734fd8a8a75581c9e7db457fffb20f
naka_d <nakad.student@gamil.com>
2015-08-27 11:13:15 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,15 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
   end
     ## ここから追記
-    
-    def create
+   
+  
+  
+   def create
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -15,6 +18,19 @@ class MessagesController < ApplicationController
       @messages = Message.all
       flash.now[:alert] = "メッセージの保存に失敗しました。"
       render 'index'
+    end
+  end
+  
+   def edit
+  end
+  
+   def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
     end
   end
   
@@ -29,6 +45,10 @@ class MessagesController < ApplicationController
      # params[:message]のパラメータで name , bodyのみを許可する。
     # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
     params.require(:message).permit(:name, :body)
+  end
+  
+   def set_message
+    @message = Message.find(params[:id])
   end
   ## ここまで
 end
